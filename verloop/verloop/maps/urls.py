523b5b7da05034c2addfde28f94b72110e054bf1
pavan kumar <pavankumarmedikonduri@gmail.com>
2026-03-18 01:01:23 +0000

    Delete verloop directory

--- verloop/verloop/maps/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import MapTemplatePage, AddressToCoordinatesConverter
-
-urlpatterns = [
-    path('coordinates/', MapTemplatePage.as_view()),
-    path('getAddressDetails/', AddressToCoordinatesConverter.as_view())
-]
